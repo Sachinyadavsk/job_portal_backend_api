5cdd1c913cb8acf9b2147a1b11035aa26309928a
Sachinyadavsk <sachinsk4832499@gmail.com>
2026-07-31 00:57:38 +0530

    packge json file change

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use(cookieParser());
 const corsOptions = {
-    origin: "https://job-portal-frontend-navy-six.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true
 }
 
