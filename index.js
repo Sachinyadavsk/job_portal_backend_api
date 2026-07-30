e5088f1710ecb319c6ef010efd2586ff9f6649ab
Sachinyadavsk <sachinsk4832499@gmail.com>
2026-07-31 00:50:40 +0530

    fontend

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use(cookieParser());
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://job-portal-frontend-navy-six.vercel.app",
     credentials: true
 }
 
